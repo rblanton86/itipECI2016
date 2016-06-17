1749c9fb550109a3f5b7d3d568a2ab8ade597a71
rblanton <rblanton@theironyard.com>
2016-06-17 16:07:55 -0500

    Added script for dev user

---
+++ itipEci2016/eciDB2016/v1/CreateDevUser.sql
@@ -0,0 +1,11 @@
+﻿CREATE LOGIN eciUser_Dev WITH PASSWORD = 'dev_user2016', 
+	CHECK_POLICY     = OFF,
+    CHECK_EXPIRATION = OFF
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'eciUser_Dev')
+BEGIN
+    CREATE USER [eciUser_Dev] FOR LOGIN [eciUser_Dev]
+    EXEC sp_addrolemember N'db_owner', N'eciUser_Dev'
+END
+GO 
