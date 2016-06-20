bb5c595808eba626d765338a6128cbee48df1406
Jennifer Graves <jem.graves@gmail.com>
2016-06-20 13:00:16 -0500

    Updated clients script with if statements and added Race/Ethnicity and linking scripts for Clients and Addresses.

--- itipEci2016/eciDB2016/v1/Clients.sql
+++ itipEci2016/eciDB2016/v1/Clients.sql
@@ -6,42 +6,81 @@ Author:
 Date: 
 	6.20.2016
 Change History:
-	
+	06-20-2016 - jmg - updated scripts with if statements
 ************************************************************************************************************/
-CREATE TABLE Clients ( 
-	clientID INT IDENTITY (1,1) PRIMARY KEY (clientID),
-	raceID INT FOREIGN KEY REFERENCES Race(raceID),
-	ethnicityID INT FOREIGN KEY REFERENCES Ethnicity(ethnicityID),
-	clientStatusID INT FOREIGN KEY REFERENCES ClientStatus(clientStatusID),
-	familyID INT FOREIGN KEY REFERENCES FamilyMember(familyID),
-	referralSourceID INT FOREIGN KEY REFERENCES ReferralSource(referralSourcID),
-	physicianID INT FOREIGN KEY REFERENCES Physician(physicianID),
-	staffID INT FOREIGN KEY REFERENCES Staff(staffID),
-	memberAddressID INT FOREIGN KEY REFERENCES Addresses(addressesID),
-	additionalContactInfoID INT FOREIGN KEY REFERENCES AdditionalContactInfo(additionalContactInfoID),
-	insuranceID INT FOREIGN KEY REFERENCES Insurance(insuranceID),
-	firstName VARCHAR(25),
-	lastName VARCHAR(25),
-	dob INT,
-	ssn INT,
-	referralSource VARCHAR(50),
-)
---identifies race of patient
-CREATE TABLE Race (
-	raceID INT IDENTITY (1,1),
-	race VARCHAR(25),
-)
---identifies ethnicity of patient
-CREATE TABLE Ethnicity (
-	ethnicityID INT IDENTITY (1,1),
-	ethnicity VARCHAR(25),
-)
--- provides information regarding patient status
-CREATE TABLE ClientStatus (
-	clientStatusID INT IDENTITY (1,1),
-	clientStatus VARCHAR(25),
-	initialDate INT,
-	dismissedDate INT,
-	dismissalReason VARCHAR(100),
-)
 
+-- Declaring variables to hold table IDs.
+DECLARE @raceID INT = 0,
+	@ethnicityID INT = 0,
+	@clientStatusID INT = 0
+
+-- Checking for the existence of foreign key columns.
+SELECT @raceID = columnID
+FROM Sys.Columns 
+WHERE objectID = (
+	SELECT objectID
+	FROM Sys.Tables
+	WHERE name = 'Race')
+
+SELECT @ethnicityID = columnID 
+FROM Sys.Columns 
+WHERE objectID = (
+	SELECT objectID
+	FROM Sys.Tables
+	WHERE name = 'Ethnicity'
+	)
+
+SELECT @clientStatusID = columnID
+FROM Sys.Columns
+WHERE objectID = (
+	SELECT objectID
+	FROM Sys.Tables
+	WHERE name = 'ClientStatus'
+	)
+
+-- Creates table if it does not already exist.
+IF @raceID = 0
+BEGIN
+	--identifies race of patient
+	CREATE TABLE Race (
+		raceID INT IDENTITY (1,1),
+		race VARCHAR(25),
+	)
+END
+
+IF @ethnicityID = 0
+BEGIN
+	--identifies ethnicity of patient
+	CREATE TABLE Ethnicity (
+		ethnicityID INT IDENTITY (1,1),
+		ethnicity VARCHAR(25),
+	)
+END
+
+IF @clientStatusID = 0
+BEGIN
+	-- provides information regarding patient status
+	CREATE TABLE ClientStatus (
+		clientStatusID INT IDENTITY (1,1),
+		clientStatus VARCHAR(25),
+		initialDate INT,
+		dismissedDate INT,
+		dismissalReason VARCHAR(100),
+	)
+END
+
+IF
+(SELECT object_id FROM sys.tables WHERE name = 'Clients') > 0
+BEGIN
+	CREATE TABLE Clients ( 
+		clientID INT IDENTITY (1,1) PRIMARY KEY (clientID),
+		raceID INT FOREIGN KEY REFERENCES Race(raceID),
+		ethnicityID INT FOREIGN KEY REFERENCES Ethnicity(ethnicityID),
+		clientStatusID INT FOREIGN KEY REFERENCES ClientStatus(clientStatusID),
+		firstName VARCHAR(25),
+		lastName VARCHAR(25),
+		dob INT,
+		ssn INT,
+		referralSource VARCHAR(50),
+	)
+END
