cde3467423f9f2eb4a942ef0ce56ddc45ca85928
TyCrane <tycrane@ymail.com>
2016-06-21 09:07:02 -0700

    Created WEB MVC and added 2 html scripts and 2 css scritps

---
+++ itipEci2016/eciWEB2016/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eciWEB2016.MvcApplication" Language="C#" %>
---
+++ itipEci2016/eciWEB2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eciWEB2016.Startup))]
+namespace eciWEB2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
