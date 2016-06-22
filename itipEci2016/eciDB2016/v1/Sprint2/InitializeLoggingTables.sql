e9cc2f14d5af491ca13fac3ca04e984af0523154
TyCrane <tycrane@ymail.com>
2016-06-22 08:27:14 -0700

    Created dummy data script. Added several stored procedures

---
+++ itipEci2016/eciDB2016/v1/Sprint2/InitializeLoggingTables.sql
@@ -0,0 +1,16 @@
+﻿/***********************************************************************************************************
+Description: Creates initial logging tables to hold error messages and time of error
+	 
+Author: 
+	Tyrell Powers-Crane 
+Date: 
+	6.21.16
+Change History:
+	
+************************************************************************************************************/
+CREATE TABLE ErrorLog (
+	errorLog int IDENTITY (1,1) PRIMARY KEY,
+	errorTime datetime,
+	errorMessage varchar (50),
+	errorProcedure varchar (25)
+	)
---
+++ itipEci2016/eciDB2016/v1/Stored_Procedures/get_Client.sql
@@ -0,0 +1,36 @@
+﻿/***********************************************************************************************************
+Description: Stored Procedure to pull information from Clients table
+	 
+Author: 
+	Tyrell Powers-Crane 
+Date: 
+	6.21.16
+Change History:
+	
+************************************************************************************************************/
+CREATE PROCEDURE [dbo].[get_Client]
+	@clientID int
+
+AS
+	BEGIN
+		BEGIN TRY
+
+			SELECT firstName, lastName, dob, ssn, referralSource
+			FROM Clients
+			WHERE @clientID = clientID
+
+		END TRY
+		BEGIN CATCH
+
+			DECLARE @timeStamp DATETIME,
+				@errorMessage VARCHAR(255),
+				@errorProcedure VARCHAR(100)	
+
+			SELECT @timeStamp = GETDATE(),
+					@errorMessage = ERROR_MESSAGE(),
+					@errorProcedure = ERROR_PROCEDURE()
+			
+			EXECUTE dbo.log_ErrorTimeStamp @timeStamp, @errorMessage, @errorProcedure
+
+		END CATCH
+	END
