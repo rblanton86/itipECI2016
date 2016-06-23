24a432dc20b7fb4923d5da5b5b726b8e04c90feb
Jennifer <jem.graves@gmail.com>
2016-06-23 17:39:05 -0500

    Updated initial Diagnosis Columns script and prepared alt_Diagnosis, created LnkClientPhysician table.

--- itipEci2016/eciDB2016/v1/Source/Schema/init_DiagnosisColumns.sql
+++ itipEci2016/eciDB2016/v1/Source/Schema/init_DiagnosisColumns.sql
@@ -6,7 +6,10 @@ Author:
 Date: 
 	06-22-2016
 Change History:
-	
+	06-23-2016 -- jmg -- Drops icd9Code and icd10Code and adds the following columns:
+			diagnosisType VARCHAR(15),
+			diagnosisCode VARCHAR(15),
+			diagnosis VARCHAR(100),
 ************************************************************************************************************/
 
 -- Declares table variable for diagnosis table
@@ -26,45 +29,74 @@ IF @diagnosis = 0
 	BEGIN
 		CREATE TABLE Diagnosis (
 			diagnosisID INT IDENTITY(1,1) PRIMARY KEY,
-			diagnosisTypeID INT FOREIGN KEY REFERENCES DiagnosisTypeID(diagnosisTypeID),
-			icd10Code VARCHAR(15),
-			icd9Code VARCHAR(15),
+			diagnosisType VARCHAR(15),
+			diagnosisCode VARCHAR(15),
+			diagnosis VARCHAR(100),
 		)
 	END
--- Because table exists, addes columns for icd10 and icd9
+-- Because table exists, addes columns for diagnosisType, diagnosisCode and diagnosis.
 ELSE
 	BEGIN
-
-		-- Checks if icd10 column exists, alters table to add if it doesn't.
-		IF EXISTS (SELECT * FROM sys.columns WHERE @diagnosis = OBJECT_ID AND name = 'icd10Code')
+		IF EXISTS (SELECT * FROM sys.columns WHERE OBJECT_ID = @diagnosis AND name = 'diagnosisType')
 			BEGIN
-
-			-- Checks if icd9 column exists, alters table to add if it doesn't.
-			IF EXISTS (SELECT * FROM sys.columns WHERE @diagnosis = OBJECT_ID AND name = 'icd9Code')
-				BEGIN
-					RETURN
-				END
-			ELSE
-				BEGIN
-					ALTER TABLE Diagnosis
-						ADD icd9Code VARCHAR(15)
-				END
+				PRINT 'diagnosisType Column exists.'
 			END
 		ELSE
 			BEGIN
-							
 				ALTER TABLE Diagnosis
-					ADD icd10Code VARCHAR(15)				
+					ADD diagnosisType VARCHAR(15)
+			END
 
-				-- Checks if icd9 column exists, alters table to add if it doesn't.
-				IF EXISTS (SELECT * FROM sys.columns WHERE @diagnosis = OBJECT_ID AND name = 'icd9Code')
-					BEGIN
-						RETURN
-					END
-				ELSE
-					BEGIN
-						ALTER TABLE Diagnosis
-							ADD icd9Code VARCHAR(15)
-					END				
+		IF EXISTS (SELECT * FROM sys.columns WHERE OBJECT_ID = @diagnosis AND name = 'diagnosisCode')
+			BEGIN
+				PRINT 'diagnosisCode Column exists.'
+			END
+		ELSE
+			BEGIN
+				ALTER TABLE Diagnosis
+					ADD diagnosisCode VARCHAR(15)
+			END
+
+		IF EXISTS (SELECT * FROM sys.columns WHERE OBJECT_ID = @diagnosis AND name = 'diagnosis')
+			BEGIN
+				PRINT 'diagnosis Column exists.'
+			END
+		ELSE
+			BEGIN
+				ALTER TABLE Diagnosis
+					ADD diagnosis VARCHAR(100)
+			END
+
+		IF EXISTS (SELECT * FROM sys.columns WHERE OBJECT_ID = @diagnosis AND name = 'icd10Code')
+			BEGIN
+				ALTER TABLE Diagnosis
+					DROP COLUMN icd10Code
+			END
+		ELSE
+			BEGIN
+				PRINT 'icd10Code Column does not exist.'
+			END
+
+		IF EXISTS (SELECT * FROM sys.columns WHERE OBJECT_ID = @diagnosis AND name = 'icd10Code')
+			BEGIN
+				ALTER TABLE Diagnosis
+					DROP COLUMN icd10Code
+			END
+		ELSE
+			BEGIN
+				PRINT 'icd10Code Column does not exist.'
+			END
+
+		IF EXISTS (SELECT * FROM sys.columns WHERE OBJECT_ID = @diagnosis AND name = 'icd_10_Code')
+			BEGIN
+				DECLARE @some NVARCHAR(100) = 'UPDATE Diagnosis SET diagnosisCode = icd_10_Code'
+				EXEC sp_executesql @some
+		
+				ALTER TABLE Diagnosis
+					DROP COLUMN icd_10_Code
+			END
+		ELSE
+			BEGIN
+				PRINT 'icd_10_Code Column does not exist.'
 			END
 	END
