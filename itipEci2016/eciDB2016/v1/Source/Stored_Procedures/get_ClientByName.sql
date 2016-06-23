db5e2a1e02fee52cfe332873e79f5679db108cab
TyCrane <tycrane@ymail.com>
2016-06-23 05:11:47 -0700

    added get client by name procedure

---
+++ itipEci2016/eciDB2016/v1/Source/Stored_Procedures/get_ClientByName.sql
@@ -0,0 +1,37 @@
+﻿/***********************************************************************************************************
+Description: Stored Procedure to pull client information from Clients table by name
+	 
+Author: 
+	Tyrell Powers-Crane 
+Date: 
+	6.22.16
+Change History:
+	
+************************************************************************************************************/
+CREATE PROCEDURE [dbo].[get_ClientByName]
+	@firstName varchar(25),
+	@lastName varchar(25)
+
+AS
+	BEGIN
+		BEGIN TRY
+
+			SELECT firstName, lastName, dob, ssn, referralSource
+			FROM Clients
+			WHERE (@firstName = firstName) AND (@lastName = lastName)
+
+		END TRY
+		BEGIN CATCH
+
+			DECLARE @timeStamp DATETIME,
+				@errorMessage VARCHAR(255),
+				@errorProcedure VARCHAR(100)	
+
+			SELECT @timeStamp = GETDATE(),
+					@errorMessage = ERROR_MESSAGE(),
+					@errorProcedure = ERROR_PROCEDURE()
+			
+			EXECUTE dbo.log_ErrorTimeStamp @timeStamp, @errorMessage, @errorProcedure
+
+		END CATCH
+	END
