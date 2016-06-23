d4b5586a3e7abd88330b5439053524bd02891154
TyCrane <tycrane@ymail.com>
2016-06-23 16:06:45 -0700

    added Comments and Physician Stored Procedures

---
+++ itipEci2016/eciDB2016/v1/Source/Stored_Procedures/get_Comments.sql
@@ -0,0 +1,40 @@
+﻿/***********************************************************************************************************
+Description: Stored Procedure that retrieves information from the Comments Table
+	 
+Author: 
+	Tyrell Powers-Crane 
+Date: 
+	6.23.16
+Change History:
+	
+************************************************************************************************************/
+CREATE PROCEDURE [dbo].[get_Comments]
+	@commentsID int
+
+
+AS
+	BEGIN
+		BEGIN TRY
+
+			SELECT comments
+			FROM Comments
+			WHERE commentsID = @commentsID
+
+		END TRY
+		BEGIN CATCH
+
+			DECLARE @timeStamp DATETIME,
+				@errorMessage VARCHAR(255),
+				@errorProcedure VARCHAR(100)	
+
+			SELECT @timeStamp = GETDATE(),
+					@errorMessage = ERROR_MESSAGE(),
+					@errorProcedure = ERROR_PROCEDURE()
+			
+			EXECUTE dbo.log_ErrorTimeStamp @timeStamp, @errorMessage, @errorProcedure
+
+		END CATCH
+	END
+
+
+
