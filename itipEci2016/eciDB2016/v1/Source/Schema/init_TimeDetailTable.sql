66e0705a8fd11f3d74217565172ef2771b0c8780
TyCrane <tycrane@ymail.com>
2016-07-05 13:40:31 -0500

    Created Time Sheet Tables

---
+++ itipEci2016/eciDB2016/v1/Source/Schema/init_TimeDetailTable.sql
@@ -0,0 +1,26 @@
+﻿/***********************************************************************************************************
+Description: Creates the Time Detail Table
+	 
+Author: 
+	Tyrell Powers-Crane 
+Date: 
+	7.05.2016
+Change History:
+	
+************************************************************************************************************/
+
+CREATE TABLE TimeDetail (
+	timeDetailID int IDENTITY (1,1) PRIMARY KEY,
+	timeHeaderID int FOREIGN KEY REFERENCES TimeHeader(timeHeaderID),
+	clientID int FOREIGN KEY REFERENCES Clients(clientID),
+	actualTime decimal,
+	eciCode varchar(5),
+	insuranceDesignation char(1),
+	cptCode varchar(5),
+	insuranceTime decimal,
+	placeOfService char(1),
+	tcm varchar(10),
+	canceled varchar(10),
+	updDate datetime,
+	deleted bit
+	)
