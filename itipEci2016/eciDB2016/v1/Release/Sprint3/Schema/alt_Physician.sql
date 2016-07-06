34c47ccfd500f0cb78aa0f5a130e6b2115d6a290
Jennifer M. Graves <jem.graves@gmail.com>
2016-07-06 06:32:38 -0500

    Added deleted bit column to Insurance, Physician and ReferralSource tables.

---
+++ itipEci2016/eciDB2016/v1/Release/Sprint3/Schema/alt_Physician.sql
@@ -0,0 +1,55 @@
+﻿/****************************************************************************
+Description: Alter Physician table.
+
+Author: Jennifer M Graves
+
+Date: 07-05-2016
+
+Change History:
+		07-05-2016 -- jmg: Added deleted bit column to table.
+****************************************************************************/
+
+-- Declares the table variable for Physician
+DECLARE @md INT = 0
+
+-- Assigns the system table ID to @md variable for later use.
+SELECT @md = (
+	SELECT OBJECT_ID
+	FROM sys.tables
+	WHERE name = 'Physician'
+)
+
+SELECT @md
+
+-- Checks if table exists.
+IF ISNULL(@md, 0) = 0
+	BEGIN
+		--Creates Physician table if it doesn't exist.
+		CREATE TABLE Physician (
+			physicianID INT IDENTITY(1,1) PRIMARY KEY(physicianID),
+			addressesID INT CONSTRAINT FK_physician_addresses FOREIGN KEY REFERENCES Addresses(addressesID),
+			additionalContactInfoID INT CONSTRAINT FK_physician_contactInfo FOREIGN KEY REFERENCES Addresses(additionalContactInfoID),
+			title VARCHAR(10),
+			firstName VARCHAR(25),
+			lastName VARCHAR(25),
+			deleted BIT
+		)
+
+		PRINT 'Added Physician table to database.'
+	END
+ELSE
+	BEGIN
+		-- Checks if deleted column exists.
+		IF EXISTS (SELECT * FROM sys.columns WHERE @md = OBJECT_ID AND name = 'deleted')
+			BEGIN
+				-- Advises DBA no column added, as already exists.
+				PRINT 'Unneeded: deleted column exists.'
+			END
+		ELSE
+			BEGIN
+				-- Creates column, advises DBA.
+				ALTER TABLE Physician
+					ADD deleted BIT
+				PRINT 'Added deleted column on Physician table.'
+			END
+	END
---
+++ itipEci2016/eciDB2016/v1/Release/Sprint3/Schema/alt_ReferralSource.sql
@@ -0,0 +1,53 @@
+﻿/****************************************************************************
+Description: Alters ReferralSource table.
+
+Author: Jennifer M Graves
+
+Date: 07-05-2016
+
+Change History:
+		07-05-2016 -- jmg: Added deleted bit column to table.
+****************************************************************************/
+
+-- Declares the table variable for ReferralSource
+DECLARE @rs INT = 0
+
+-- Assigns the system table ID to @rs variable for later use.
+SELECT @rs = (
+	SELECT OBJECT_ID
+	FROM sys.tables
+	WHERE name = 'ReferralSource'
+)
+
+SELECT @rs
+
+-- Checks if table exists.
+IF ISNULL(@rs, 0) = 0
+	BEGIN
+		--Creates ReferralSource table if it doesn't exist.
+		CREATE TABLE ReferralSource (
+			referralSourceID INT IDENTITY(1,1) PRIMARY KEY(referralSourceID),
+			additionalContactInfoID INT CONSTRAINT FK_referralSource_contactInfo FOREIGN KEY REFERENCES AdditionalContactInfo(additionalContactInfoID),
+			referralSourceTypeID INT CONSTRAINT FK_referralSource_referralSourceType FOREIGN KEY REFERENCES ReferralSourceType(referralSourceTypeID),
+			addressesID INT CONSTRAINT FK_referralSource_addresses FOREIGN KEY REFERENCES Addresses(addressesID),
+			deleted BIT
+		)
+
+		PRINT 'Added ReferralSource table to database.'
+	END
+ELSE
+	BEGIN
+		-- Checks if deleted column exists.
+		IF EXISTS (SELECT * FROM sys.columns WHERE @rs = OBJECT_ID AND name = 'deleted')
+			BEGIN
+				-- Advises DBA no column added, as already exists.
+				PRINT 'Unneeded: deleted column exists.'
+			END
+		ELSE
+			BEGIN
+				-- Creates column, advises DBA.
+				ALTER TABLE ReferralSource
+					ADD deleted BIT
+				PRINT 'Added deleted column on ReferralSource table.'
+			END
+	END
